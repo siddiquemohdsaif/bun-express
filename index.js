86e84a6a647613823fdbb54b5d9717679d7bffef
Shabir <mdshabirshaik@gmail.com>
2023-09-16 17:48:05 +0530

    optimize routes

--- index.js
+++ index.js
@@ -8,28 +8,33 @@ app.use((req) => {
 });
 app.use(BunExpress.json());
 
+
 // Routes
-app.get('/', async (req, res) => {
+app.get('/delaySecond', async (req, res) => {
     // Simulate some asynchronous operation
     await new Promise(resolve => setTimeout(resolve, 1000));
     res.send("Hello from the home page!");
 });
 
-app.get('/ddd', (req, res) => {
-    res.send("Hello from /ddd!" + req.timestamp);
-});
-
-app.post('/jsonBody/:ssss', async (req, res) => {
-    res.send("body.data :" + req.BODY.data + " param:" +req.params.sss);
-});
 
 app.get('/users/:userId', (req, res) => {
     const userId = req.params.userId;
     res.send(`User ID: ${userId}`);
 });
 
-app.get('/path/*', async (req, res) => {
+app.get('/test/:id/:name', (req, res) => {
+    const id = req.params.id;
+    const name = req.params.name;
+    res.send(`User ID: ${id}  name: ${name}`);
+});
+
+
+app.get('/pathHandle/*', async (req, res) => {
     res.send( " path:"+req.url);
+});
+
+app.get('/*', (req, res) => {
+    res.send("my custom : 404 not found : " + req.pathname);
 });
 
 // Start the server
